0271ba193b484b4ba0d0ee8ab9bc2116dc77107e
waley-code <oyewaleadeosun@yahoo.com>
2023-09-03 09:58:47 +0100

    complet task 3

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,5 +1,5 @@
-import redisClient from "../utils/redis";
-import dbClient from "../utils/db";
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   /**
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import controllerRouting from "./routes/index";
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
