8771a00d21731f3f711972271b92d34b433b7054
waley-code <oyewaleadeosun@yahoo.com>
2023-09-03 10:28:58 +0100

    fix usercontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,10 @@
-import { ObjectId } from "mongodb";
-import sha1 from "sha1";
-import Queue from "bull";
-import dbClient from "../utils/db";
-import userUtils from "../utils/user";
+import { ObjectId } from 'mongodb';
+import sha1 from 'sha1';
+import Queue from 'bull';
+import dbClient from '../utils/db';
+import userUtils from '../utils/user';
 
-const userQueue = new Queue("userQueue");
+const userQueue = new Queue('userQueue');
 
 class UsersController {
   /**
@@ -13,16 +13,16 @@ class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
 
-    if (!email) return response.status(400).send({ error: "Missing email" });
+    if (!email) return response.status(400).send({ error: 'Missing email' });
 
     if (!password) {
-      return response.status(400).send({ error: "Missing password" });
+      return response.status(400).send({ error: 'Missing password' });
     }
 
     const emailExists = await dbClient.usersCollection.findOne({ email });
 
     if (emailExists) {
-      return response.status(400).send({ error: "Already exist" });
+      return response.status(400).send({ error: 'Already exist' });
     }
 
     const sha1Password = sha1(password);
@@ -35,7 +35,7 @@ class UsersController {
       });
     } catch (err) {
       await userQueue.add({});
-      return response.status(500).send({ error: "Error creating user." });
+      return response.status(500).send({ error: 'Error creating user.' });
     }
 
     const user = {
@@ -60,7 +60,7 @@ class UsersController {
       _id: ObjectId(userId),
     });
 
-    if (!user) return response.status(401).send({ error: "Unauthorized" });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const processedUser = { id: user._id, ...user };
     delete processedUser._id;
--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,19 @@ function controllerRouting(app) {
 
   // App Controller
 
-  // should return if Redis is alive and if the DB is alive
+  // return if Redis is alive and if the DB is alive
   router.get('/status', (req, res) => {
     AppController.getStatus(req, res);
   });
 
-  // should return the number of users and files in DB
+  // return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
   });
 
   // User Controller
 
-  // should create a new user in DB
+  // create a new user in DB
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
   });
